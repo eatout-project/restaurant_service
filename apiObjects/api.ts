6663fccee553ee562b34c5655804247f6559a41c
Sathveekan <sathveekan23@gmail.com>
2022-11-09 22:43:29 +0100

    EAT-8 - Implemented restaurant login response

--- apiObjects/api.ts
+++ apiObjects/api.ts
@@ -1,11 +1,27 @@
+export interface RestaurantLogin {
+    email: string;
+    password: string
+}
+
 export interface RestaurantApiObject {
     email: string;
-    name: string;
+    restaurantName: string;
     description: string;
     address: AddressApiObject;
     image?: string;
     restaurantId?: number;
     menu?: RestaurantMenuApiObject;
+}
+
+export interface RestaurantResponseApiObject {
+    id: number;
+    email: string;
+    restaurantName: string;
+    description: string;
+    restaurantAddress: AddressResponseApiObject;
+    image?: string;
+    restaurantId?: number;
+    restaurantMenu?: RestaurantMenuApiObject;
 }
 
 export interface RestaurantMenuApiObject {
@@ -33,4 +49,21 @@ export interface AddressApiObject {
     zipCode: number,
     city: string
     floor?: number
+}
+
+export interface AddressResponseApiObject {
+    id: number,
+    restaurantId: number,
+    streetName: string,
+    houseNumber: number,
+    zipCode: number,
+    city: string
+    floor?: number
+}
+
+export interface LoginApiObject {
+    id: number,
+    email: string,
+    restaurantName: string,
+    description: string
 }
